2e550a75e27b112580d4b0532fc03aa8c3b9db71
FiRafiq <rafiqf03@gmail.com>
2024-06-01 12:26:04 +0600

    database structure

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['author','teacher','student']);
+            $table->enum('role', ['author','teacher','student']);//author, teacher, student
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
